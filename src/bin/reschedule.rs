fca4fb6524257bf126e4a078ec898c28e5e02aea
Algermissen Jan <jan.algermissen@metro.de>
2016-11-30 20:58:56 +0100

    First working version of reschedule (using boxes)

--- src/bin/reschedule.rs
+++ src/bin/reschedule.rs
@@ -10,14 +10,19 @@ use futures::Future;
 use futures::IntoFuture;
 use futures::Then;
 use futures::Map;
+use std::result::Result;
+use std::io;
 use futures::Async;
 use futures::future::Ok;
 use futures::Poll;
 use std::thread;
 use std::cell::Cell;
+use std::boxed::Box;
+//use std::boxed::FnBox;
 
 use tokio_core::reactor::{Core, Timeout};
 
+#[derive(Debug)]
 struct Error {
     code: i32
 }
@@ -28,106 +33,111 @@ impl Error {
     }
 }
 
-// A future as return type of getAsynch() method below.
-//struct After {
-//    d: Duration,
-//    s: State
+
+//struct MyFuture<S: Clone> {
+//    v: S
 //}
 //
-//impl Future for After {
-//    type Item = State;
+//impl<S: Clone> Future for MyFuture<S>
+//{
+//    type Item = S;
 //    type Error = Error;
 //
-//    fn poll(&mut self) -> Poll<State, Error> {
-//        Ok(Async::Ready(self.s))
+//    fn poll(&mut self) -> Poll<S, Error> {
+//        //self.get().poll()
+//        Ok(Async::Ready(self.v.clone()))
 //    }
 //}
 
+//impl<S: Clone> IntoFuture for MyFuture<S> {
+//    type Future = MyFuture<S>;
+//    type Item = S;
+//    type Error = Error;
+//
+//    fn into_future(self) -> MyFuture<S> {
+//        self
+//    }
+//}
+
+//impl<F, R> Future for Lazy<F, R>
+//where F: FnOnce() -> R,
+//R: IntoFuture,
+
+
 enum Cmd<S: Clone> {
     Start(S),
-    //    Immediately(Duration, S),
+    //    Run(Box<FnBox(Handle) -> MyFuture<S>>),
+    //Run(Box<FnOnce(Handle) -> MyFuture<S>>),
+    Run(fn(handle: Handle) -> Box<Future<Item = S, Error = ()>>),
     After(Duration, S)
 }
 
 
 // A struct that holds some state and a handle on
 // which to run its worker function.
+#[derive(Debug)]
 struct Actor<S: Clone> {
-    dur: Duration,
+    //dur: Duration,
     state: S,
-    //fu: fn(S) -> Future<Item = S, Error = Error>,
-    fu: fn(S) -> Cmd<S>,
+    //fu: fn(S) -> Cmd<S>,
 }
 
 impl<S: 'static + Clone> Actor<S> {
-    // Create a new S and schedule its worker function
-    //    fn new(handle: Handle, start: S, f: fn(S) -> Future<Item = S, Error = Error>) -> Actor<S> {
-    fn new(handle: Handle, start: S, f: fn(S) -> Cmd<S>) -> Actor<S> {
-        let d = Duration::from_millis(5000);
-        let a: Actor<S> = Actor { dur: d, state: start.clone(), fu: f };
-        let cmd = Cmd::Start(start.clone());
-        Actor::nextf2(handle, cmd, f);
-        //handle.spawn(fut);
+    fn new(handle: Handle, start_state: S, guts: fn(S) -> Cmd<S>) -> Actor<S> {
+        //        let d = Duration::from_millis(5000);
+        //        let a: Actor<S> = Actor { dur: d, state: start.clone(), fu: f };
+        let a: Actor<S> = Actor { state: start_state.clone() };
+        let cmd = Cmd::Start(start_state.clone());
+        Actor::run_guts(handle, cmd, guts);
         a
     }
 
-    fn nextf2(handle: Handle, cmd: Cmd<S>, f: fn(S) -> Cmd<S>) {
+    fn run_guts(handle: Handle, cmd: Cmd<S>, guts: fn(S) -> Cmd<S>) {
         let futu = match cmd {
-            Cmd::Start(s) => {
+            Cmd::Start(state) => {
                 let timeout = Timeout::new(Duration::from_millis(5000), &handle).unwrap();
-                let x = handle.clone();
-                let s2 = s.clone();
-                let f2 = f.clone();
-                let futu = timeout.then(move |r| {
-                    println!("Worker says start");
-                    match r {
-                        Ok(_) => {
-                            let cmd2 = f2(s2);
-                            Actor::nextf2(x, cmd2, f);
-                            futures::future::ok::<(), ()>(())
-                        },
-                        Err(_) => futures::future::ok::<(), ()>(()),
-                    }
+                let handle_copy = handle.clone();
+                let state_copy = state.clone();
+                let guts_copy = guts.clone();
+                let fut = timeout.then(move |r| {
+                    println!("******************************** START **********");
+                    let next_cmd = guts_copy(state_copy);
+                    Actor::run_guts(handle_copy, next_cmd, guts_copy);
+                    // TODO match on r
+                    futures::future::ok::<(), ()>(())
                 });
-                handle.spawn(futu);
+                handle.spawn(fut);
             },
-            Cmd::After(d, s) => {
+            Cmd::After(d, state) => {
                 let timeout = Timeout::new(d, &handle).unwrap();
-                let x = handle.clone();
-                let s2 = s.clone();
-                let f2 = f.clone();
-                let futu = timeout.then(move |r| {
-                    println!("Worker says after");
-                    match r {
-                        Ok(_) => {
-                            let cmd2 = f2(s2);
-                            Actor::nextf2(x, cmd2, f);
-                            futures::future::ok::<(), ()>(())
-                        },
-                        Err(_) => futures::future::ok::<(), ()>(()),
-                    }
+                let handle_copy = handle.clone();
+                let state_copy = state.clone();
+                let guts_copy = guts.clone();
+                let fut = timeout.then(move |r| {
+                    println!("******************************** AFTER **********");
+                    let next_cmd = guts_copy(state_copy);
+                    Actor::run_guts(handle_copy, next_cmd, guts_copy);
+                    // TODO match on r
+                    futures::future::ok::<(), ()>(())
                 });
-                handle.spawn(futu);
+                handle.spawn(fut);
             },
-            //            Cmd::Run(rf) => {
-            //                let timeout = Timeout::new(d, &handle).unwrap();
-            //                let x = handle.clone();
-            //
-            //                let s2 = s.clone();
-            //                let f2 = f.clone();
-            //                let futu = rf.then(move |r| {
-            //                    println!("Worker says run");
-            //                    match r {
-            //                        Ok(_) => {
-            //                            let cmd2 = f2(s2);
-            //                            Actor::nextf2(x, cmd2, f);
-            //                            futures::future::ok::<(), ()>(())
-            //                        },
-            //                        Err(_) => futures::future::ok::<(), ()>(()),
-            //                    }
-            //                });
-            //                handle.spawn(futu);
-            //            }
+            Cmd::Run(work_future) => {
+                let handle_copy = handle.clone();
+                let handle_copy2 = handle.clone();
+                //                let state_copy = state.clone();
+                let guts_copy = guts.clone();
+                let fut0: Box<Future<Item = S, Error = ()>> = work_future(handle_copy);
+                let fut = fut0.then(move |x: Result<S, ()>| {
+                    let new_state = x.unwrap();
+                    println!("******************************** RUN **********");
+                    let next_cmd = guts_copy(new_state);
+                    Actor::run_guts(handle_copy2, next_cmd, guts_copy);
+                    // TODO match on r
+                    futures::future::ok::<(), ()>(())
+                });
+                handle.spawn(fut);
+            }
         };
     }
 
@@ -140,75 +150,45 @@ impl<S: 'static + Clone> Actor<S> {
 #[derive(Copy, Clone)]
 enum State {
     Start(i32),
-    Foo,
+    Foo(i32),
     End
 }
 
-
-//fn testGetFromUpstream<R, F>(handle: &Handle, d: Duration, v: R) -> Map<Timeout, F>
-//    where R: Clone, F: FnOnce(()) -> R,
-//{
-//    let timeout = Timeout::new(d, handle).unwrap();
-//    let futu = timeout.map(|r| { v });
-//    futu
-//}
-
-fn testSimulateRemoteGetValue<R, F>(handle: &Handle, d: Duration, f: F) -> Map<Timeout, F>
-    where R: Clone, F: FnOnce(()) -> R,
-{
-    let timeout = Timeout::new(d, handle).unwrap();
-    let fut: Map<Timeout, F> = timeout.map(f);
-    fut
-}
-
-//fn map<F, U>( self , f: F) -> Map < Self, F>
-//where F: FnOnce( Self::Item) -> U,
-//Self: Sized,
-//{
-//assert_future::< U, Self::Error, _> (map::new( self, f))
-//}
-
-//type FOO = fn(()) -> i32;
-//
-//fn testGetFromUpstream(handle: &Handle, d: Duration, v: i32) -> Map<Timeout, fn(()) -> i32> {
-//    let timeout = Timeout::new(d, handle).unwrap();
-//    let c: fn(()) -> i32 = |r| { v };
-//    timeout.map(c)
-//}
 
 fn main() {
     // A thread on which background workers will be run.
     let worker_thread = thread::spawn(move || {
         let mut core = Core::new().unwrap();
 
-        //        fn add_10<F>(f: F) -> Map<F, fn(i32) -> i32>
-        //            where F: Future<Item = i32>,
-        //        {
-        //            fn do_map(i: i32) -> i32 { i + 10 }
-        //            f.map(do_map)
-        //        }
-        //
-        //        fn doIt<S>(s:S) -> Map<F, fn(i32) -> i32>
-        //            where F: Future<Item = S>,
-        //        {
-        //
-        //        }
 
-        //        fn update(state: State) -> Future<Item = State, Error = Error> {
+        //        let z = testSimulateRemoteGetValue(&(core.handle()), |()| { State::End }).then(|_| {
+        //            println!("XXXXXXXXXXXXX");
+        //            futures::future::ok::<(), ()>(())
+        //        });
+        //        core.handle().spawn(z);
+
+        fn doFoo(handle: Handle) -> Box<Future<Item = State, Error = ()>> {
+            println!("Starting dofoo >>>> =======================================");
+            let f = testSimulateRemoteGetValue(&handle, |()| { State::End });
+            //Box::new(f.map(|x| { State::End }).map_err(|e| { () }))
+            Box::new(f.map(|x| {
+                println!("completed dofoo <<<< =======================================");
+                State::End
+            }).map_err(|e| { () }))
+        }
+
         fn update(state: State) -> Cmd<State> {
             match state {
                 State::Start(s) => {
-                    println!("start");
-                    Cmd::After(Duration::new(1, 0), State::Foo)
+                    println!("start - foo in 1 sec");
+                    Cmd::After(Duration::new(1, 0), State::Foo(20))
                 },
-                //                State::Foo => {
-                //                    println!("foo");
-                //                    Cmd::Run(|| {
-                //
-                //                    })
-                //                },
+                State::Foo(s) => {
+                    println!("foo - run doFoo now");
+                    Cmd::Run(doFoo)
+                },
                 State::End => {
-                    println!("end");
+                    println!("end - end, run end egain in 1 sec");
                     Cmd::After(Duration::new(1, 0), State::End)
                 },
             }
@@ -231,4 +211,13 @@ fn main() {
     // These threads will eventually use the state the workers work on.
 
     worker_thread.join().unwrap();
+}
+
+
+fn testSimulateRemoteGetValue<R, F>(handle: &Handle, f: F) -> Map<Timeout, F>
+    where R: Clone, F: FnOnce(()) -> R,
+{
+    let timeout = Timeout::new(Duration::from_millis(5000), handle).unwrap();
+    let fut: Map<Timeout, F> = timeout.map(f);
+    fut
 }
